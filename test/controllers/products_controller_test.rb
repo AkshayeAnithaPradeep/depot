bc2f2c420b4cead3358c8e6bf154a02bd9f0a46c
Akshaye Anitha Pradeep <akshayeanithapradeep@Akshayes-MacBook-Pro.local>
2019-09-03 22:07:26 -0400

    Added ajax

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -21,7 +21,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: @update }
+      post products_url, params: {product: @update}
     end
 
     assert_redirected_to product_url(Product.last)
@@ -38,15 +38,20 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: @update }
+    patch product_url(@product), params: {product: @update}
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
+  end
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
-
     assert_redirected_to products_url
   end
 end
